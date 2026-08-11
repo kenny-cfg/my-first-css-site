1458155eac49ea5c3f27a4023820f6322c12ec1f
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-08-11 19:16:36 +0100

    Some more JS with CSS, and some caSIng comments

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ console.log('I am running');
 const firstParagraph = document.getElementById("first-paragraph");
 
 firstParagraph.style.backgroundColor = "pink";
+firstParagraph.style.fontSize = "xx-small";
+
+/* camelCase */
+/* PascalCase */
+/* snake_case */
+/* kebab-case */
