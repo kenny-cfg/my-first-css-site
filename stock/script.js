3c84b929a318270d5f7e010c8973694be7b2598b
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-08-11 20:27:05 +0100

    Button on click event wired up

--- stock/script.js
+++ stock/script.js
@@ -1,1 +1,11 @@
-console.log('STOCK JS');
+let stock = 10;
+const stockButton = document.getElementById("stock-button");
+
+const updateStockIndicator = () => {
+  console.log(`Stock is ${stock}`)
+}
+
+stockButton.addEventListener("click", () => {
+  stock--;
+  updateStockIndicator();
+})
