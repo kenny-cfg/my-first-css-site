584b5eee25384891ad27bf750399164a534acc1a
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-08-11 20:28:11 +0100

    Stock inicator wired up

--- stock/script.js
+++ stock/script.js
@@ -1,11 +1,14 @@
 let stock = 10;
 const stockButton = document.getElementById("stock-button");
+const stockIndicator = document.getElementById("stock-indicator");
 
 const updateStockIndicator = () => {
-  console.log(`Stock is ${stock}`)
+  stockIndicator.textContent = `Stock is ${stock}`;
 }
 
 stockButton.addEventListener("click", () => {
   stock--;
   updateStockIndicator();
 })
+
+updateStockIndicator();
