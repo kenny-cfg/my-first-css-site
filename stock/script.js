f58152c7747b2d6a8f31368136cee11dedf94bd9
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-08-11 20:30:55 +0100

    Bonus task

--- stock/script.js
+++ stock/script.js
@@ -1,13 +1,25 @@
 let stock = 10;
 const stockButton = document.getElementById("stock-button");
 const stockIndicator = document.getElementById("stock-indicator");
+const messageElement = document.getElementById("message");
 
 const updateStockIndicator = () => {
   stockIndicator.textContent = `Stock is ${stock}`;
 }
 
+const setMessage = (message) => {
+  messageElement.textContent = message;
+  messageElement.style.backgroundColor = "red";
+  messageElement.style.color = "yellow";
+}
+
 stockButton.addEventListener("click", () => {
-  stock--;
+  if (stock > 0) {
+    stock--;
+  }
+  if (stock === 0) {
+    setMessage("Out of stock");
+  }
   updateStockIndicator();
 })
 
