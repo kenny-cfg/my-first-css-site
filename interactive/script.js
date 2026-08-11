5fde76a3c0f844093eaf5489e2e268d8067a438d
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-08-11 19:48:33 +0100

    Count working

--- interactive/script.js
+++ interactive/script.js
@@ -1,3 +1,13 @@
-function buttonClicked() {
-  console.log("You've clicked the button")
+let counter = 0;
+const counterElement = document.getElementById("counter");
+
+function updateCounterElement() {
+  counterElement.textContent = `Count is ${counter}`;
 }
+
+function buttonClicked() {
+  counter++;
+  updateCounterElement();
+}
+
+updateCounterElement();
